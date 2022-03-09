b7fc78cf87cad3a8b4f65c1fcc705884287f2d8c
carterpager <carterpager@gmail.com>
2022-03-09 01:20:59 -0700

    Fixed date logging format

--- make-run.sh
+++ make-run.sh
@@ -9,11 +9,11 @@ makerun="./gasctld/greenctld --az-device /dev/azimuth --el-device /dev/elevation
 #echo "$process"
 if ps ax | grep -v grep | grep $process > /dev/null
 then
-    echo `date +"%Y-%M-%d %T"`" - Nominal" >> gasctld/gasctld-log.txt
+    echo `date +"%Y-%m-%d %T"`" - Nominal" >> gasctld/gasctld-log.txt
     exit
 else
     $makerun &
-    echo `date +"%Y-%M-%d %T"`" - Restarted" >> gasctld/gasctld-log.txt
+    echo `date +"%Y-%m-%d %T"`" - Restarted" >> gasctld/gasctld-log.txt
 fi
 
 exit
